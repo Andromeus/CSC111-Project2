c295ced5a3abe69fb15e32612eb55a23a56a3074
ARKLightning06 <64155125+ARKLightning06@users.noreply.github.com>
2026-03-25 16:24:26 -0400

    Implemented many functions in game_logic.py
    In AlignQuattroGame class, implemented get_available_columns, make_move, check_win (which needs to be cleaned up with help functions, and could probably be more efficient), get_outcome, and get_row_from_available_columns. In Piece class implemented simple getter and setter for _piece_type. Implemented a Player abstract class and a RandomPlayer and HumanPlayer subclasses. Also implemented run_games and run_game functions

--- game_logic.py
+++ game_logic.py
@@ -2,12 +2,16 @@
 
 from __future__ import annotations
 
+import random
+import copy
+
 
 class AlignQuattroGame:
     """
     A class representing an Align Quattro Game
 
     Instance Attributes:
+      - outcome: a string representing the outcome of the game (red win, yellow win, tie, or in progress)
       - _board: a 6 x 7 2d array of Piece objects, where self._board[0][0] is represents the top left of the board
       and self._board[5][6] represents the bottom right corner of the board.
       - _valid_moves: a dictionary mapping each column (integers 0-6) to the lowest available row
@@ -15,6 +19,7 @@ class AlignQuattroGame:
       - _is_red_move: bool for whether it is red player's turn or not
 
     Representation Invariants:
+        - self.outcome in {'red win', 'yellow win', 'tie', 'in progress'}
         - len(self._board) == 6 and len(self._board[0]) == 7
         - all([x in self._valid_moves for x in range(0, 7)])
         - all([-1 <= self._valid_moves[y] <= 5 for y in range(0,7)])
@@ -22,15 +27,14 @@ class AlignQuattroGame:
     ***DOCTESTS HERE EVENTUALLY***
 
     """
-
+    outcome: str
     _board: list[list[Piece]]
     _valid_moves: dict[int, int]
     _is_red_move: bool
 
     def __init__(self, board: list[list[Piece]] = None,
-                 is_red_move: bool = True) -> None:
+                 is_red_move: bool = True, game_outcome: str = 'in progress') -> None:
         """Initializes AlignQuattroGame"""
-
         if board is not None:
             self._board = board
         else:
@@ -42,6 +46,151 @@ class AlignQuattroGame:
                 for j in range(0, 7):
                     self._board[i].append(Piece(i, j))
         self._is_red_move = is_red_move
+        self.outcome = game_outcome
+
+    def get_available_columns(self) -> list[int]:
+        """Returns a list of integers representing columns still open for moves (so 0-6)"""
+        return [x for x in self._valid_moves if self._valid_moves[x] >= 0]
+
+    def make_move(self, col: int) -> None:
+        """Makes a move in an AlignQuattroGame, including four steps:
+        Changes the piece_type of a piece from empty to red or yellow at the lowest available row in the given
+        column of self._board,
+        Toggles self._is_red_move to update whose turn it is,
+        Updates self._valid_moves by subtracting one from the specified column to show that row has been filled, and
+        Executes self.check_win to potentially update self.outcome if the game is ended by this move.
+
+        Preconditions:
+            - col in self.get_available_columns()
+
+        """
+        row = self._valid_moves[col]
+        if self._is_red_move:
+            player = 'red'
+        else:
+            player = 'yellow'
+        self._board[row][col].set_piece_type(player)
+        self._valid_moves[col] -= 1
+        self.check_win(row, col)
+        self._is_red_move = not self._is_red_move
+
+    def check_win(self, r, c) -> None:
+        """Checks for a win or a tie based on where a piece was placed. Assumes the piece was the last piece to
+        be placed, and all pieces are up to date. Changes self.outcome to 'red win' 'yellow win' or 'tie', or
+        leaves it as in progress if no wins or ties are found.
+
+        Checks for ties by referencing self._valid_moves
+
+        Checks for wins by searching through four possible win conditions:
+        1. Vertical
+        2. Horizontal
+        3. Postive Diagonal
+        4. Negative Diagonal
+
+        Preconditions:
+            - 0 <= r <= 5
+            - 0 <= c <= 6
+        """
+        aligned_so_far = 1
+        i = 1
+        if self._is_red_move:
+            player = 'red'
+            win = 'red win'
+        else:
+            player = 'yellow'
+            win = 'yellow win'
+        # check vertical (only checks down, not up, nothing can be placed on top of the last played piece)
+        while i < 4 and r + i < 6:
+            if self._board[r + i][c].get_piece_type() == player:
+                aligned_so_far += 1
+                i += 1
+            else:
+                i = 4
+
+        if aligned_so_far >= 4:
+            self.outcome = win
+            return
+        else:
+            aligned_so_far = 1
+            i = 1
+
+        # check horizontal
+        while i < 4 and c + i < 7:
+            if self._board[r][c + i].get_piece_type() == player:
+                aligned_so_far += 1
+                i += 1
+            else:
+                i = 4
+        i = 1
+        while i < 4 and c + i > -1:
+            if self._board[r][c - i].get_piece_type() == player:
+                aligned_so_far += 1
+                i += 1
+            else:
+                i = 4
+
+        if aligned_so_far >= 4:
+            self.outcome = win
+            return
+        else:
+            aligned_so_far = 1
+            i = 1
+
+        # check positive diagonal
+        while i < 4 and c + i < 7 and r + i < 6:
+            if self._board[r + i][c + i].get_piece_type() == player:
+                aligned_so_far += 1
+                i += 1
+            else:
+                i = 4
+        i = 1
+        while i < 4 and c - i > -1 and r - i > -1:
+            if self._board[r - i][c - i].get_piece_type() == player:
+                aligned_so_far += 1
+                i += 1
+            else:
+                i = 4
+
+        if aligned_so_far >= 4:
+            self.outcome = win
+            return
+        else:
+            aligned_so_far = 1
+            i = 1
+
+        # check negative diagonal
+        while i < 4 and c + i < 7 and r - i > -1:
+            if self._board[r - i][c + i].get_piece_type() == player:
+                aligned_so_far += 1
+                i += 1
+            else:
+                i = 4
+        i = 1
+        while i < 4 and c - i > -1 and r + i < 6:
+            if self._board[r + i][c - i].get_piece_type() == player:
+                aligned_so_far += 1
+                i += 1
+            else:
+                i = 4
+
+        if aligned_so_far >= 4:
+            self.outcome = win
+            return
+        else:
+            aligned_so_far = 1
+            i = 1
+
+        # check for a tie
+        if all([self._valid_moves[x] == -1 for x in self._valid_moves]):
+            self.outcome = 'tie'
+
+    def get_outcome(self) -> str:
+        """Returns self.outcome"""
+        return self.outcome
+
+    def get_row_from_available_columns(self, input_col: int) -> int:
+        """Returns the lowest row for the given column, or self._valid_moves[input_col]"""
+        return self._valid_moves[input_col]
 
 
 class Piece:
@@ -54,8 +203,8 @@ class Piece:
 
     Representation Invariants:
       - self._piece_type in {'red', 'yellow', 'empty'}
-      - 0 <= self._row_pos <= 6
-      - 0 <= self._col_pos <= 7
+      - 0 <= self._row_pos <= 5
+      - 0 <= self._col_pos <= 6
 
     """
 
@@ -65,12 +214,119 @@ class Piece:
 
     def __init__(self, row_val: int, col_val: int, p_type: str='empty') -> None:
         """Initializes Piece as empty by default, or else as a specified type, either 'red' or 'yellow'"""
-
-        self.piece_type = p_type
+        self._piece_type = p_type
         self._row_pos = row_val
         self._col_pos = col_val
+
+    def get_piece_type(self) -> str:
+        """Returns self._piece_type"""
+        return self._piece_type
+
+    def set_piece_type(self, set_type: str) -> None:
+        """Sets self._piece_type to the specified type
+
+        Preconditions:
+            - set_type in {'red', 'yellow', 'empty'}
+        """
+        self._piece_type = set_type
+
+
+class Player:
+    """An abstract class representing an AlignQuattro AI.
+
+    This class can be subclassed to implement different strategies for playing AlignQuattro.
+    """
+
+    def make_move(self, game: AlignQuattroGame) -> int:
+        """Make a move given the current game.
+
+        Preconditions:
+            - There is at least one valid move for the given game
+        """
+        raise NotImplementedError
+
+
+class RandomPlayer(Player):
+    """An AlignQuattro player whose strategy is to make a random move each turn."""
+
+    def make_move(self, game: AlignQuattroGame) -> int:
+        """Make a move given the current game.
+
+        Preconditions:
+            - There is at least one valid move for the given game
+        """
+        possible_moves = game.get_available_columns()
+        return random.choice(possible_moves)
+
+
+class HumanPlayer(Player):
+    """An AlignQuattro player which take real human input to make human decided moves each turn."""
+
+    def make_move(self, game: AlignQuattroGame) -> int:
+        """Waits for human player to type a number 1 - 7 to select a column."""
+        choice = input("Choose a column, 1-7: ")
+        options = {'1', '2', '3', '4', '5', '6', '7'}
+        while choice not in options:
+            print("That was an invalid option; try again.")
+            choice = input("\nChoose a column, 1-7: ")
+        return int(choice) - 1
+
+
+
+################################################################################
+# Functions for running games
+################################################################################
+
+def run_games(n: int, red: Player, yellow: Player) -> None:
+    """Run n games using the given Players
+
+    Preconditions:
+        - n >= 1
+    """
+    stats = {'red win': 0, 'yellow win': 0, 'tie': 0}
+    for i in range(0, n):
+        red_copy = copy.deepcopy(red)
+        yellow_copy = copy.deepcopy(yellow)
+
+        winner, _ = run_game(red_copy, yellow_copy)
+        stats[winner] += 1
+        print(f'Game {i} winner: {winner}')
+
+    for outcome in stats:
+        print(f'{outcome}: {stats[outcome]}/{n} ({100.0 * stats[outcome] / n:.2f}%)')
+
+
+def run_game(red: Player, yellow: Player) -> tuple[str, list[tuple[str, int, int]]]:
+    """Run a Minichess game between the two given players.
+
+    Return the outcome: either 'red win', 'yellow win', or 'tie'.
+    """
+    game = AlignQuattroGame()
+
+    move_sequence = []
+    current_player = red
+    while game.get_outcome() == "in progress":
+
+        col_input = current_player.make_move(game)
+        row_input = game.get_row_from_available_columns(col_input)
+        game.make_move(col_input)
+        if current_player is red:
+            current_player = yellow
+            player_str = "red"
+        else:
+            current_player = red
+            player_str = "yellow"
+        move_sequence.append((player_str, row_input, col_input))
+    return game.get_outcome(), move_sequence
 
 
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
+
+    # import python_ta
+    # python_ta.check_all(config={
+    #     'max-line-length': 120,
+    #     'disable': ['static_type_checker'],
+    #     'extra-imports': ['random', 'a2_minichess', 'a2_game_tree']
+    # })
