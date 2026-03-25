21c6c183b87a4218db4df10170646edb8fd50c8c
Andromeus <andrew.li3056@gmail.com>
2026-03-25 16:31:26 -0400

    replaced old white circle with a red or yellow circle depending on who made the move

--- game_display.py
+++ game_display.py
@@ -10,11 +10,11 @@ running = True
 # draw empty board
 screen.fill("blue")
 circle_radius = 55
-White = (255, 255, 255, 255)
+White = (255, 255, 255)
 for row in range(6):
-    center_y = (row + 1) * (10 + 55) + 55 * row
+    center_y = (row + 1) * (10 + circle_radius) + circle_radius * row
     for column in range(7):
-        center_x = (column + 1) * (65 + 55) + 55 * column
+        center_x = (column + 1) * (65 + circle_radius) + circle_radius * column
         pygame.draw.circle(screen, White, (center_x, center_y), circle_radius)
 pygame.display.flip()
 
@@ -25,10 +25,26 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # wipe the spot of the move with a color to wipe it away from last frame
+    # assuming move is a position in the board(row, column) as well as a color
+    # get move in some way
+    # move = row, column, color in some way
 
+    # draw a new circle representing moved piece over the old circle representing empty piece
 
-    # RENDER YOUR GAME HERE
+    # making transparent background rectangle surface
+    circle_surface = pygame.Surface((circle_radius * 2, circle_radius * 2), pygame.SRCALPHA) #
+    # pygame.SRCALPHA makes it transparent
+
+    # drawing the circle on the background surface
+    # center_x = (column + 1) * (65 + 55) + 55 * column
+    # center_y = (row + 1) * (10 + 55) + 55 * row
+    # pygame.draw.circle(circle_surface, color, (center_x, center_y), circle_radius)
+
+    # getting rectangle with circle to be replaced in it based on where move was made
+    # rect_left = 65 * (column + 1) + 110 * column
+    # rect_top = 10 * (row + 1) + 110 * row
+    # replaced_rect = circle_surface.get_rect(rect_left, rect_top, circle_radius * 2, circle_radius * 2)
+    # screen.blit(circle_surface, replaced_rect)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
