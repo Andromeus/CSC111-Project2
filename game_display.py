0b45f769d6104cfe22d1daa714eecff8c9891905
Andromeus <andrew.li3056@gmail.com>
2026-03-26 15:19:42 -0400

    changed game display into 4 functions
    intialize game: intializes a screen and timer, draws an empty game on the screen
    
    update game: get info for moves and add it to pending moves
    
    draw circle: replaces a white circle with a colored circle depending on info of move passed in: row, column, color
    
    game_loop: makes a game loop run, it will constantly check if there is moves in pending moves and draw a circle depending on the moves in pending moves

--- game_display.py
+++ game_display.py
@@ -1,57 +1,76 @@
 import game_logic
 import pygame
+pending_moves = []
 
 
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
+def intialize_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
 
-# draw empty board
-screen.fill("blue")
-circle_radius = 55
-White = (255, 255, 255)
-for row in range(6):
-    center_y = (row + 1) * (10 + circle_radius) + circle_radius * row
-    for column in range(7):
-        center_x = (column + 1) * (65 + circle_radius) + circle_radius * column
-        pygame.draw.circle(screen, White, (center_x, center_y), circle_radius)
-pygame.display.flip()
+    # draw empty board
+    screen.fill("blue")
+    circle_radius = 55
+    White = (255, 255, 255)
+    for row in range(6):
+        center_y = (row + 1) * (10 + circle_radius) + circle_radius * row
+        for column in range(7):
+            center_x = (column + 1) * (65 + circle_radius) + circle_radius * column
+            pygame.draw.circle(screen, White, (center_x, center_y), circle_radius)
+    pygame.display.flip()
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    return screen, clock, circle_radius
 
-    # assuming move is a position in the board(row, column) as well as a color
-    # get move in some way
-    # move = row, column, color in some way
 
-    # draw a new circle representing moved piece over the old circle representing empty piece
+def update_game(row, col, color):
+    pending_moves.append((row, col, color))
+
+
+def draw_circle(screen, row: int, column: int, circle_radius: int, type: str):
 
     # making transparent background rectangle surface
-    circle_surface = pygame.Surface((circle_radius * 2, circle_radius * 2), pygame.SRCALPHA) #
+    circle_surface = pygame.Surface((circle_radius * 2, circle_radius * 2), pygame.SRCALPHA)  #
     # pygame.SRCALPHA makes it transparent
 
     # drawing the circle on the background surface
-    # center_x = (column + 1) * (65 + 55) + 55 * column
-    # center_y = (row + 1) * (10 + 55) + 55 * row
-    # pygame.draw.circle(circle_surface, color, (center_x, center_y), circle_radius)
+    center_x = (column + 1) * (65 + 55) + 55 * column
+    center_y = (row + 1) * (10 + 55) + 55 * row
+    if type == "red":
+        color = (255, 0, 0)
+    else:
+        color = (0, 255, 0)
+
+    pygame.draw.circle(circle_surface, color, (center_x, center_y), circle_radius)
 
     # getting rectangle with circle to be replaced in it based on where move was made
-    # rect_left = 65 * (column + 1) + 110 * column
-    # rect_top = 10 * (row + 1) + 110 * row
-    # replaced_rect = circle_surface.get_rect(rect_left, rect_top, circle_radius * 2, circle_radius * 2)
-    # screen.blit(circle_surface, replaced_rect)
+    rect_left = 65 * (column + 1) + 110 * column
+    rect_top = 10 * (row + 1) + 110 * row
+    replaced_rect = circle_surface.get_rect(rect_left, rect_top, circle_radius * 2, circle_radius * 2)
+    screen.blit(circle_surface, replaced_rect)
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+def game_loop(screen, clock):
+    running = True
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-pygame.quit()
+        while len(pending_moves) > 0:
+            row, col, color = pending_moves.pop(0)
+            draw_circle(screen, row, col, 55, color)
+
+        pygame.display.flip()
+        clock.tick(60)
+
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        clock.tick(60)  # limits FPS to 60
+    pygame.quit()
+
 
 if __name__ == '__main__':
     import doctest
@@ -63,6 +82,4 @@ if __name__ == '__main__':
     #     'disable': ['static_type_checker'],
     #     'extra-imports': []
     # })
-
-
 
